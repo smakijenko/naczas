3f371760aa43dfde9296746af54b98143de8e215
Stanisław Makijenko <makijenko02@gmail.com>
2025-02-11 22:44:45 +0100

    Initial Commit

---
+++ naczas/naczasApp.swift
@@ -0,0 +1,17 @@
+//
+//  naczasApp.swift
+//  naczas
+//
+//  Created by Stanisław Makijenko on 11/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct naczasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
